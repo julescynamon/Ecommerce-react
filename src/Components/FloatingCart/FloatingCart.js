a1c399dfee9a00ad22a23c72c398957f0c194043
cynamon jules <julescweb@mail.com>
2022-05-16 17:28:41 +0200

    router finis

---
+++ src/Components/FloatingCart/FloatingCart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CartIcon from './shopping-cart.svg';
+import './FloatingCart.css';
+
+export default function FloatingCart() {
+    return (
+        <div>
+        
+        </div>
+    )
+}
