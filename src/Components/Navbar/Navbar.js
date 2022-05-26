83862b60c4502a45795946f1a6c053428703a777
julescynamon <87892309+julescynamon@users.noreply.github.com>
2022-05-26 17:45:58 +0200

    maj

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <nav className="nav-top">
-            <Link to="/">ACCEUIL</Link>
+            <Link to="/Ecommerce-react">ACCEUIL</Link>
             <Link to="/produits">PRODUITS</Link>
             <Link to="/contact">CONTACT</Link>
         </nav>
